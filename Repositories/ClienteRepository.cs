98d2afa5c2f78b31d665259a0a077c436a9d316e
Thalita Sylvia Marques <thalita.sylvia@gmail.com>
2019-12-19 14:36:43 -0300

    Cliente Repository

---
+++ Repositories/ClienteRepository.cs
@@ -0,0 +1,53 @@
+﻿using ECommerce.Database;
+using ECommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private ECommerceContext _banco;
+
+        public ClienteRepository(ECommerceContext banco)
+        {
+            _banco = banco;
+        } 
+        public void Atualizar(Cliente cliente)
+        {
+            _banco.Update(cliente);
+            _banco.SaveChanges();
+        }
+
+        public void Cadastrar(Cliente cliente)
+        {
+            _banco.Add(cliente);
+            _banco.SaveChanges();
+        }
+
+        public void Excluir(int Id)
+        {
+            Cliente cliente = ObterCliente(Id);
+            _banco.Remove(cliente);
+            _banco.SaveChanges();
+        }
+
+        public Cliente Login(string Email, string Senha)
+        {
+            Cliente cliente = _banco.Clientes.Where(m => m.Email == Email && m.Senha == Senha).First();
+            return cliente;
+        }
+
+        public Cliente ObterCliente(int Id)
+        {
+            return _banco.Clientes.Find(Id);
+        }
+
+        public List<Cliente> ObterTodosClientes()
+        {
+            return _banco.Clientes.ToList();
+        }
+    }
+}
---
+++ Repositories/IClienteRepository.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Repositories
+{
+    interface IClienteRepository
+    {
+        Cliente Login(string Email, string Senha);
+
+        void Cadastrar(Cliente cliente);
+        void Atualizar(Cliente cliente);
+        void Excluir(int Id);
+        Cliente ObterCliente(int Id);
+        List<Cliente> ObterTodosClientes();
+
+    }
+}
